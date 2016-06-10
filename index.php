ed6e0edc59bb0e7230f03de755d28f98e21aeb80
jsavargas <jsavargas@gmail.com>
2016-06-10 16:07:18 -0400

    preg_match

--- index.php
+++ index.php
@@ -1,17 +1,18 @@
 <?php
 
-
-
-
-
-
-
 $pag = file_get_contents("http://www.emol.com/economia/");
 
 
-echo $pag;
+//echo $pag;
 
-echo "Hola Mundo";
+
+preg_match("/(Dolar Observado).+?valor_divisas.+?<span.+?>(.+?)<\/span>/im",$pag,$matchs);
+
+
+
+print_r($matchs);
+
+//echo "Hola Mundo";
 
 
 
